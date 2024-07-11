fd594791820d558e24628d01be624f909a9b16b7
291207 <99871513+291207@users.noreply.github.com>
2024-07-11 19:55:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,4 @@
 # devggn
 # Note if you are trying to deploy on vps then directly fill values in ("")
 
-from os import getenv
 
-API_ID = int(getenv("API_ID", "23476439"))
-API_HASH = getenv("API_HASH", "1626e884119a29dbccbb78e39b48128f")
-BOT_TOKEN = getenv("BOT_TOKEN", "7214405015:AAHeKGw4aIlpZpyqTV1owUypwkeY50vqQLs")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5993556795").split()))
-MONGO_DB = getenv("MONGO_DB", "mongodb+srv://ggn:ggn@ggn.upuljx5.mongodb.net/?retryWrites=true&w=majority&appName=ggn")
-LOG_GROUP = getenv("LOG_GROUP", "-1002175816545")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002207508831"))
